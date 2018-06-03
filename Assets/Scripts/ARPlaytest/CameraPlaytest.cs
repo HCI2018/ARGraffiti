404c586b9388040abb06ab2cae0a61ebccf033a5
Yang Liu <lykavin@hotmail.com>
2018-06-03 16:26:47 +0800

    test scene improved, added cam playtest

---
+++ Assets/Scripts/ARPlaytest/CameraPlaytest.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlaytest : MonoBehaviour {
+
+	public Transform arCamera;
+	public Transform editorCamera;
+
+	public Transform mountToCamera;
+
+    public bool enableEditorCamera = false;
+
+	// bool updated = false;
+
+	// Use this for initialization
+	void Awake () {
+#if !UNITY_EDITOR
+        InitArCamera();
+#else
+        // when we are in editor...
+        if (enableEditorCamera)
+            InitEditorCamera();
+        else
+            InitArCamera();
+#endif
+
+	}
+
+    void InitArCamera()
+    {
+        // when we are on the iOS device...
+        arCamera.gameObject.SetActive(true);
+        editorCamera.gameObject.SetActive(false);
+        if (mountToCamera != null)
+        {
+            Transform[] childs = new Transform[mountToCamera.childCount];
+            for (int i = 0; i < mountToCamera.childCount; ++i)
+            {
+                childs[i] = mountToCamera.GetChild(i);
+            }
+            foreach (Transform child in childs)
+            {
+                child.SetParent(arCamera);
+            }
+        }
+    }
+
+    void InitEditorCamera()
+    {
+        editorCamera.gameObject.SetActive(true);
+        arCamera.gameObject.SetActive(false);
+        if (mountToCamera != null)
+        {
+            Transform[] childs = new Transform[mountToCamera.childCount];
+            for (int i = 0; i < mountToCamera.childCount; ++i)
+            {
+                childs[i] = mountToCamera.GetChild(i);
+            }
+            foreach (Transform child in childs)
+            {
+                child.SetParent(editorCamera);
+            }
+        }
+    }
+
+    //	// Update is called once per frame
+    //	void Update () {
+    //		
+    //	}
+}
---
+++ Assets/Scripts/ARPlaytest/DrawerEditorTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerEditorTest : MonoBehaviour {
+
+    public ARSpriteDrawer drawer;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            drawer.StartDrawing();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            drawer.StopDrawing();
+        }
+    }
+}
