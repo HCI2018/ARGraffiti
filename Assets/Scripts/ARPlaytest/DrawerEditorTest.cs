2443b7451245a82213b29f480007dcee98c9dde7
Yang Liu <lykavin@hotmail.com>
2018-06-06 16:57:36 +0800

    Added simple artboard factory & artboard manager

--- Assets/Scripts/ARPlaytest/DrawerEditorTest.cs
+++ Assets/Scripts/ARPlaytest/DrawerEditorTest.cs
@@ -1,21 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class DrawerEditorTest : MonoBehaviour {
 
     public ARSpriteDrawer drawer;
-
+    bool drawing = false;
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
             drawer.StartDrawing();
+            drawing = true;
         }
 
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0) && drawing)
         {
             drawer.StopDrawing();
+            drawing = false;
         }
     }
 }
---
+++ Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector3 eulerAngles = transform.rotation.eulerAngles;
+
+		Quaternion rot = Quaternion.AngleAxis(Random.Range(0f, 360f), Vector3.forward);
+
+		// eulerAngles.y = Random.Range(0f, 360f);
+
+		// Quaternion newRot = Quaternion.Euler(eulerAngles);
+		transform.rotation *= rot ;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
